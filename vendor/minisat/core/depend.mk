ec8f77f9bbbc37d70182927924b42d1e0d2dbec0
ChuanHD <hodanhchuan@gmail.com>
2015-09-23 14:32:00 +0700

    - New minisat for os x

---
+++ vendor/minisat/core/depend.mk
@@ -0,0 +1,28 @@
+/Users/chuanhd/Documents/workspace/Others/minisat/core/Main.o /Users/chuanhd/Documents/workspace/Others/minisat/core/Main.or /Users/chuanhd/Documents/workspace/Others/minisat/core/Main.od /Users/chuanhd/Documents/workspace/Others/minisat/core/Main.op: /Users/chuanhd/Documents/workspace/Others/minisat/core/Main.cc \
+  /Users/chuanhd/Documents/workspace/Others/minisat/core/../utils/System.h \
+  /Users/chuanhd/Documents/workspace/Others/minisat/core/../utils/../mtl/IntTypes.h \
+  /Users/chuanhd/Documents/workspace/Others/minisat/core/../utils/ParseUtils.h \
+  /Users/chuanhd/Documents/workspace/Others/minisat/core/../utils/Options.h \
+  /Users/chuanhd/Documents/workspace/Others/minisat/core/../utils/../mtl/Vec.h \
+  /Users/chuanhd/Documents/workspace/Others/minisat/core/../utils/../mtl/../mtl/XAlloc.h \
+  /Users/chuanhd/Documents/workspace/Others/minisat/core/../core/Dimacs.h \
+  /Users/chuanhd/Documents/workspace/Others/minisat/core/../core/SolverTypes.h \
+  /Users/chuanhd/Documents/workspace/Others/minisat/core/../mtl/Alg.h \
+  /Users/chuanhd/Documents/workspace/Others/minisat/core/../mtl/Map.h \
+  /Users/chuanhd/Documents/workspace/Others/minisat/core/../mtl/Alloc.h \
+  /Users/chuanhd/Documents/workspace/Others/minisat/core/../core/Solver.h \
+  /Users/chuanhd/Documents/workspace/Others/minisat/core/../mtl/Heap.h
+/Users/chuanhd/Documents/workspace/Others/minisat/core/Solver.o /Users/chuanhd/Documents/workspace/Others/minisat/core/Solver.or /Users/chuanhd/Documents/workspace/Others/minisat/core/Solver.od /Users/chuanhd/Documents/workspace/Others/minisat/core/Solver.op: \
+  /Users/chuanhd/Documents/workspace/Others/minisat/core/Solver.cc \
+  /Users/chuanhd/Documents/workspace/Others/minisat/core/../mtl/Sort.h \
+  /Users/chuanhd/Documents/workspace/Others/minisat/core/../mtl/../mtl/Vec.h \
+  /Users/chuanhd/Documents/workspace/Others/minisat/core/../mtl/../mtl/IntTypes.h \
+  /Users/chuanhd/Documents/workspace/Others/minisat/core/../mtl/../mtl/XAlloc.h \
+  /Users/chuanhd/Documents/workspace/Others/minisat/core/../core/Solver.h \
+  /Users/chuanhd/Documents/workspace/Others/minisat/core/../mtl/Heap.h \
+  /Users/chuanhd/Documents/workspace/Others/minisat/core/../mtl/Alg.h \
+  /Users/chuanhd/Documents/workspace/Others/minisat/core/../utils/Options.h \
+  /Users/chuanhd/Documents/workspace/Others/minisat/core/../utils/../utils/ParseUtils.h \
+  /Users/chuanhd/Documents/workspace/Others/minisat/core/../core/SolverTypes.h \
+  /Users/chuanhd/Documents/workspace/Others/minisat/core/../mtl/Map.h \
+  /Users/chuanhd/Documents/workspace/Others/minisat/core/../mtl/Alloc.h
